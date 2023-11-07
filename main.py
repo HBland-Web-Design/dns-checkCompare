597f1ee45d17bf578008a53efaa1161bb51ca8d0
hazzakins <harry.bland@outlook.com>
2023-11-07 21:23:25 +0000

    Starting Build out on Main (may change)

--- main.py
+++ main.py
@@ -1,2 +1,46 @@
 #!/bin/python3
+import argparse
 
+
+###
+# Load Environment Variables
+###
+def loadENV():
+
+    return 
+
+###
+# Load Arguments
+###
+def getArgs():
+    parser = argparse.ArgumentParser(
+                    prog='HBWD DNS CheckCompare',
+                    description='Checks a list of DNS Records and Alerts when different to Known Good Cache',
+                    epilog='WIKI Available at the REPO https://github.com/HBland-Web-Design/dns-checkCompare')
+    parser.add_argument('-e', '--environment',
+                        action='', required=False,
+                        nargs=1, default=None, 
+                        help='Sets an overide Environment File to default')
+    parser.add_argument('-r', '--reset',
+                        action='', required=False,
+                        nargs=1, default=None, 
+                        help='Resets the Cache to known good')
+    # parser.add_argument('-r', '--reset',
+    #                     action='', required=False,
+    #                     nargs=1, default=None, 
+    #                     help='Resets the Cache to known good')
+    return parser.parse_args()
+
+###
+# Main Load
+###
+def main():
+    args = getArgs()
+
+    env = loadEnv()
+
+###
+# Start the checks
+###
+if __name__ == '__main__':
+    main()
