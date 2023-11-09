7f79411ed38640b2efdaa776fb0c9a55772703f4
Harry Bland <60017963+hazzakins@users.noreply.github.com>
2023-11-09 18:42:11 +0000

    new module in use and working on domain calc

--- resolve_query.py
+++ resolve_query.py
@@ -1,9 +1,19 @@
 import pydig
+import tldextract
 
 class NoResponse(Exception):
     def __init__(self, message=None):
         self.message = message
         super().__init__(message)
+
+def get_root_domain(url):
+    extracted = tldextract.extract(url)
+    return f"{extracted.domain}.{extracted.suffix}"
+
+# Example usage
+url = "https://subdomain.example.com/path/to/page"
+root_domain = get_root_domain(url)
+print(f"Root domain: {root_domain}")
 
 def resolveQuery(record, recordType):
 
@@ -12,7 +22,13 @@ def resolveQuery(record, recordType):
         if queryAnswer == []:
             raise NoResponse("No Value")
         else:
-            return queryAnswer
+            response = {}
+            response["domain"] = 
+            response["record"] = record
+            response["recordType"] = recordType
+            response["recordValue"] = queryAnswer
+
+            return response
+
     except NoResponse as err:
         print(f"Fail: {err}")
-    
